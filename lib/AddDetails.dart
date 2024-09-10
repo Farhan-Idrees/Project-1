151ba004ad6b061c20dbd3e4c16f5099b5361261
Farhan-Idrees <farhanidrees321@gmail.com>
2024-09-10 23:04:34 +0500

    Final

---
+++ lib/AddDetails.dart
@@ -0,0 +1,210 @@
+// // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+// import 'dart:io';
+
+// import 'package:cameye/AddCam.dart';
+// import 'package:cameye/Custom_widgets/CustomFormWidgets.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+// class AddDetails extends StatefulWidget {
+//   const AddDetails({super.key});
+
+//   @override
+//   State<AddDetails> createState() => _AddDetailsState();
+// }
+
+// class _AddDetailsState extends State<AddDetails> {
+//   final _formKey = GlobalKey<FormState>();
+//   final _emailController = TextEditingController();
+//   final _passwordController = TextEditingController();
+
+//   // File? _image;
+//   // Future<void> _pickImage() async {
+//   //   final picker = ImagePicker();
+//   //   final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+//   //   setState(() {
+//   //     if (pickedFile != null) {
+//   //       _image = File(pickedFile.path);
+//   //     } else {
+//   //       _image = null;
+//   //     }
+//   //   });
+//   // }
+// // Image Func
+//   File? _image;
+
+//   Future<void> _pickImage() async {
+//     final picker = ImagePicker();
+//     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+//     setState(() {
+//       if (pickedFile != null) {
+//         _image = File(pickedFile.path);
+//       } else {
+//         _image = null;
+//       }
+//     });
+//   }
+
+// // Data Upload
+//   Future<void> UploadVerify(String email, String password) async {
+//     if (_image == null) {
+//       // Handle case where image is null
+//       ScaffoldMessenger.of(context)
+//           .showSnackBar(SnackBar(content: Text('Please select an image')));
+//       return;
+//     }
+
+//     try {
+//       TaskSnapshot taskSnapshot = await FirebaseStorage.instance
+//           .ref("User Photo")
+//           .child(email)
+//           .putFile(_image!);
+
+//       String Url = await taskSnapshot.ref.getDownloadURL();
+
+//       await FirebaseFirestore.instance
+//           .collection("Admin Details")
+//           .doc(email)
+//           .set({
+//         "Email": email,
+//         "Password": password,
+//         "Image": Url,
+//       });
+//       if (mounted) {
+//         ScaffoldMessenger.of(context)
+//             .showSnackBar(SnackBar(content: Text('User added successfully')));
+//       }
+//     } catch (e) {
+//       // Handle error
+//       ScaffoldMessenger.of(context)
+//           .showSnackBar(SnackBar(content: Text('Failed to add details: $e')));
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Add Details"),
+//         centerTitle: true,
+//       ),
+//       body: SingleChildScrollView(
+//         child: Center(
+//           child: Column(
+//             children: [
+//               SizedBox(
+//                 height: 50,
+//               ),
+//               Text(
+//                 "Verify your details",
+//                 style: TextStyle(
+//                   fontWeight: FontWeight.bold,
+//                   fontSize: 24,
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 30,
+//               ),
+//               Form(
+//                 key: _formKey,
+//                 child: Padding(
+//                   padding: const EdgeInsets.all(10.0),
+//                   child: CustomFormField(
+//                     hintText: "Enter your email",
+//                     icon: Icons.email_outlined,
+//                     controller: _emailController,
+//                     fieldname: "Email",
+//                     validator: (value) {
+//                       if (value == null || value.isEmpty) {
+//                         return 'Please enter your Email';
+//                       }
+//                       return null;
+//                     },
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(
+//                 height: 30,
+//               ),
+//               GestureDetector(
+//                 onTap: _pickImage,
+//                 child: CircleAvatar(
+//                   radius: 75,
+//                   backgroundColor: Colors.grey[300],
+//                   backgroundImage: _image != null ? FileImage(_image!) : null,
+//                   child: _image == null
+//                       ? Icon(
+//                           Icons.person,
+//                           size: 75,
+//                           color: Colors.grey[700],
+//                         )
+//                       : null,
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Text(
+//                   "Upload your photo",
+//                   style: TextStyle(fontWeight: FontWeight.bold),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(10.0),
+//                 child: CustomFormField(
+//                   hintText: "Enter your password",
+//                   icon: Icons.password_sharp,
+//                   controller: _passwordController,
+//                   fieldname: "Password",
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter your password';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//               ),
+//               Center(
+//                 child: Padding(
+//                   padding: EdgeInsets.all(8.0),
+//                   child: SizedBox(
+//                     width: double.infinity,
+//                     child: TextButton(
+//                       style: ButtonStyle(
+//                           backgroundColor: WidgetStatePropertyAll(Colors.black),
+//                           foregroundColor: WidgetStatePropertyAll(Colors.white),
+//                           alignment: Alignment.center),
+//                       onPressed: () {
+//                         if (_formKey.currentState!.validate()) {
+//                           UploadVerify(
+//                               _emailController.text, _passwordController.text);
+//                           Navigator.pushReplacement(
+//                             context,
+//                             MaterialPageRoute(
+//                               builder: (context) => AddCam(),
+//                             ),
+//                           );
+//                         } else {}
+//                       },
+//                       child: Text(
+//                         "Verify",
+//                         style: TextStyle(
+//                           fontSize: 24,
+//                           fontWeight: FontWeight.bold,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
